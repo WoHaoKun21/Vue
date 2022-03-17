63e7c16ec5e487e2022e39f9b3eece89b56bb318
wohaokun21 <609914130@qq.com>
2022-03-17 22:01:27 +0800

    finish city css

--- src/components/city/City.vue
+++ src/components/city/City.vue
@@ -2,15 +2,19 @@
   <div>
     <city-header />
     <city-search />
+    <city-List />
+    <city-alphabet />
   </div>
 </template>
 
 <script>
 import CityHeader from "./components/Header.vue";
 import CitySearch from "./components/Search.vue";
+import CityList from "./components/List.vue";
+import CityAlphabet from "./components/Alphabet.vue";
 export default {
   name: "CitY",
-  components: { CityHeader, CitySearch },
+  components: { CityHeader, CitySearch, CityList, CityAlphabet },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue';
 import router from './router';
 import "./assets/styles/iconfont.css";
 import "./assets/styles/reset.css";
+import "./assets/styles/border.css";
 Vue.config.productionTip = false;
 Vue.use(VueAwesomeSwiper);// 使用这个轮播插件
 new Vue({
