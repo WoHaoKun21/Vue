99a64ffbcffa13b2fd0df0ee7ad6d2f4c2e291b9
wohaokun21 <609914130@qq.com>
2022-03-18 10:16:18 +0800

    ajax city

--- src/components/city/City.vue
+++ src/components/city/City.vue
@@ -2,8 +2,8 @@
   <div>
     <city-header />
     <city-search />
-    <city-List />
-    <city-alphabet />
+    <city-List :cities="cities" :hot="hotCities" />
+    <city-alphabet :cities="cities" />
   </div>
 </template>
 
@@ -12,9 +12,30 @@ import CityHeader from "./components/Header.vue";
 import CitySearch from "./components/Search.vue";
 import CityList from "./components/List.vue";
 import CityAlphabet from "./components/Alphabet.vue";
+import axios from "axios";
 export default {
   name: "CitY",
   components: { CityHeader, CitySearch, CityList, CityAlphabet },
+  data() {
+    return {
+      cities: {}, // 存储的是A B C D所对应的名字
+      hotCities: [], // 存储的热门城市
+    };
+  },
+  methods: {
+    getCityInfo() {
+      axios.get("./api/city.json").then((res) => {
+        this.handleGetCityInfoSucc(res.data.data);
+      });
+    },
+    handleGetCityInfoSucc(res) {
+      this.cities = res.cities; // 当前城市
+      this.hotCities = res.hotCities; // 热门城市
+    },
+  },
+  mounted() {
+    this.getCityInfo();
+  },
 };
 </script>
 
--- src/components/city/components/List.vue
+++ src/components/city/components/List.vue
@@ -12,78 +12,21 @@
       <div class="area">
         <div class="title border-topbottom">热门城市</div>
         <div class="button-list">
-          <div class="button-wrapper">
-            <div class="button">保定</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">保定</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">保定</div>
-          </div>
-          <div class="button-wrapper">
-            <div class="button">保定</div>
+          <div class="button-wrapper" v-for="item in hot" :key="item.id">
+            <div class="button">{{ item.name }}</div>
           </div>
         </div>
       </div>
-      <div class="area">
-        <div class="title border-topbottom">A</div>
+      <div class="area" v-for="(item, inde) in cities" :key="inde">
+        <div class="title border-topbottom">{{ inde }}</div>
         <div class="item-list">
-          <div class="item border-topbottom">阿拉尔</div>
-          <div class="item border-topbottom">阿拉尔</div>
-          <div class="item border-topbottom">阿拉尔</div>
-          <div class="item border-topbottom">阿拉尔</div>
-          <div class="item border-topbottom">阿拉尔</div>
-        </div>
-      </div>
-      <div class="area">
-        <div class="title border-topbottom">A</div>
-        <div class="item-list">
-          <div class="item border-topbottom">阿拉尔</div>
-          <div class="item border-topbottom">阿拉尔</div>
-          <div class="item border-topbottom">阿拉尔</div>
-          <div class="item border-topbottom">阿拉尔</div>
-          <div class="item border-topbottom">阿拉尔</div>
-        </div>
-      </div>
-      <div class="area">
-        <div class="title border-topbottom">A</div>
-        <div class="item-list">
-          <div class="item border-topbottom">阿拉尔</div>
-          <div class="item border-topbottom">阿拉尔</div>
-          <div class="item border-topbottom">阿拉尔</div>
-          <div class="item border-topbottom">阿拉尔</div>
-          <div class="item border-topbottom">阿拉尔</div>
-        </div>
-      </div>
-      <div class="area">
-        <div class="title border-topbottom">A</div>
-        <div class="item-list">
-          <div class="item border-topbottom">阿拉尔</div>
-          <div class="item border-topbottom">阿拉尔</div>
-          <div class="item border-topbottom">阿拉尔</div>
-          <div class="item border-topbottom">阿拉尔</div>
-          <div class="item border-topbottom">阿拉尔</div>
-        </div>
-      </div>
-      <div class="area">
-        <div class="title border-topbottom">A</div>
-        <div class="item-list">
-          <div class="item border-topbottom">阿拉尔</div>
-          <div class="item border-topbottom">阿拉尔</div>
-          <div class="item border-topbottom">阿拉尔</div>
-          <div class="item border-topbottom">阿拉尔</div>
-          <div class="item border-topbottom">阿拉尔</div>
-        </div>
-      </div>
-      <div class="area">
-        <div class="title border-topbottom">A</div>
-        <div class="item-list">
-          <div class="item border-topbottom">阿拉尔</div>
-          <div class="item border-topbottom">阿拉尔</div>
-          <div class="item border-topbottom">阿拉尔</div>
-          <div class="item border-topbottom">阿拉尔</div>
-          <div class="item border-topbottom">阿拉尔</div>
+          <div
+            class="item border-topbottom"
+            v-for="innerItem in item"
+            :key="innerItem.id"
+          >
+            {{ innerItem.name }}
+          </div>
         </div>
       </div>
     </div>
@@ -94,6 +37,10 @@
 import Bscroll from "better-scroll";
 export default {
   name: "CityList",
+  props: {
+    cities: Object, // 城市
+    hot: Array, // 热门城市
+  },
   mounted() {
     new Bscroll(".wrapper", {
       // 用来进行设置滚动
